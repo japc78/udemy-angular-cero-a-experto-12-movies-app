3a89360c671b18e469e574bc4be9532e8f27c657
japc <japc.grafico@gmail.com>
2021-01-19 11:36:38 +0100

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieComponent } from './pages/movie/movie.component';
+import { FindComponent } from './pages/find/find.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'movie/:id', component: MovieComponent},
+  { path: 'search/:text', component: FindComponent},
+  { path: '**', redirectTo: '/home'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
